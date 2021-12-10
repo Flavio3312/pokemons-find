ba5b0ef70f1c157dff0b3192112ce4cccf240f2f
Flavio Perez <feperez3312@gmail.com>
2021-12-10 11:25:04 -0300

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
       Creado por <a href="https://flavio3312.github.io/Card/">Flavio Perez</a>
     </div>
     <div>
-      <a href="https://github.com/Flavio3312/pokemon-finder">GitHub</a>
+      <a href="https://github.com/Flavio3312/pokemons-find">GitHub</a>
     </div>
   </footer>
 );
